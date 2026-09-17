e5d94bef0ada9263b6d23321a34ec51bdf9573e5
krites143 <kriteshgavit@gmail.com>
2026-09-17 22:31:38 +0530

    Add files via upload

---
+++ test_dim.ps1
@@ -0,0 +1,5 @@
+$b = [System.IO.File]::ReadAllBytes('d:\kotwari\kotwari_eagle_transparent.png')
+$w = [System.BitConverter]::ToInt32(@($b[19], $b[18], $b[17], $b[16]), 0)
+$h = [System.BitConverter]::ToInt32(@($b[23], $b[22], $b[21], $b[20]), 0)
+Write-Host " Dimensions: $w x $h\
+Remove-Item 'test_dim.ps1'
